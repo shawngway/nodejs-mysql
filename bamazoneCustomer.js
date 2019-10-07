42270cf5659c2a50553078b5d83b421466c7e389
ShawnG <shawngway@gmail.com>
2019-10-07 18:07:56 -0400

    checking stock then updating

--- bamazoneCustomer.js
+++ bamazoneCustomer.js
@@ -1,6 +1,9 @@
 const mysql = require("mysql");
 const inquirer = require("inquirer");
 
+
+let stock = 0;
+
 const connection = mysql.createConnection({
     host: "localhost",
 
@@ -18,17 +21,17 @@ connection.connect((err) => {
     productMenu();
 });
 
-function productMenu(){
+function productMenu() {
     console.log("These are the products in stock\n");
-    connection.query("SELECT * FROM products", function(err, res) {
+    connection.query("SELECT * FROM products", function (err, res) {
         if (err) throw err;
-        for (let i = 0; i < res.length; i++){
-        console.log("product id: " + res[i].item_id);
-        console.log("product name: " + res[i].product_name);
-        console.log("department: " + res[i].department_name);
-        console.log("price: " + res[i].price);
-        console.log("In stock: " + res[i].stock_quantity);
-        console.log("---------------");
+        for (let i = 0; i < res.length; i++) {
+            console.log("product id: " + res[i].item_id);
+            console.log("product name: " + res[i].product_name);
+            console.log("department: " + res[i].department_name);
+            console.log("price: " + res[i].price);
+            console.log("In stock: " + res[i].stock_quantity);
+            console.log("---------------");
         }
         createInvoice();
     })
@@ -54,12 +57,31 @@ function createInvoice() {
 };
 
 
-function readInvoice(choice, amount){
+function readInvoice(choice, orderNum) {
     console.log("product id: " + choice)
-    console.log("product quantity wanting to purchase: " + amount);
-    stockCheck(choice, amount);
+    console.log("product quantity wanting to purchase: " + orderNum);
+    stockCheck(choice, orderNum);
 };
 
-function stockCheck(id, stock){
+function stockCheck(id, orderSize) {
+    connection.query("SELECT * FROM products WHERE item_id =" + id, function (err, res) {
+        if (err) throw err;
+        // console.log(res);
+        order = parseInt(orderSize);
+        stock = parseInt(res[0].stock_quantity)
+        console.log("There are only " + stock + " " + res[0].product_name + " in stock");
+        if (order > stock) {
+            console.log("So sorry we do not have enough of those in stock! :(");
+            //create a set timeout so this is shown before we re show the initialize
+        } else{
+            console.log("Your order has been place!")
+            //as before place a set timeout so this is shown then we re show the initialize
+            stockUpdate(id, order, stock);
+        }
+    })
+}
 
+function stockUpdate(item, order, stock){
+    // console.log(item, order, stock);
+    
 }
